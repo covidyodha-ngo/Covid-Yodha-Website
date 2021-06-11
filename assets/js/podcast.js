1350ebe49cb8cc70d7d20272473ab791ce96621f
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2021-06-11 22:16:14 +0530

    fixes

--- assets/js/podcast.js
+++ assets/js/podcast.js
@@ -6,6 +6,22 @@ fetch(parseUrl + RSS_URL)
     .then(response => response.json())
     .then(data => {
         console.log(data);
+        console.log(data.items);
+        const item = data.items;
+
+        console.log(item[0].author);
+
+        console.log(item[0].thumbnail);
+
+        console.log(item[0].title);
+
+        console.log(item[0].enclosure.link);
+
+        // const renderRssItems = (item) => (
+        //     <div class="rss-container">
+        //         <img src={item.thumbnail} alt="" />
+        //     </div>
+        // )
         // const items = data.querySelectorAll("item");
         // const renderRssItems = (items) => (
         //     <div class="rss-container">
