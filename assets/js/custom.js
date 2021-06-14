5a5705f94a85747d4b7642e3b9da5b9976e70f49
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2021-06-14 19:42:26 +0530

    fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-	$('.chat_icon').click(function() {
-		$('.chat_box').toggleClass('active');
-	});
+// $(document).ready(function () {
+// 	$('.chat_icon').click(function () {
+// 		$('.chat_box').toggleClass('active');
+// 	});
 
-	$('.my-conv-form-wrapper').convform({selectInputStyle: 'disable'})
-});
+// 	$('.my-conv-form-wrapper').convform({ selectInputStyle: 'disable' })
+// });
